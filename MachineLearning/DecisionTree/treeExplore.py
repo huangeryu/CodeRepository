083ae368bd094650b55b04676ac7c782306df2d6
huangyu2 <huangyu2@pset.suntec.net>
2018-01-08 19:09:34 +0800

    regTrees

---
+++ MachineLearning/DecisionTree/treeExplore.py
@@ -0,0 +1,13 @@
+from numpy import *
+from Tkinter import *
+import regTrees
+def reDrew(tolS,tolN):
+    pass
+def drawNewTree():
+    pass
+root=Tk()
+Label(root,text="plot place Holder").grid(row=0,columspan=3)
+Label(root,text="tolN").grid(row=1,column=0)
+tolNentry=Entry(root)
+tolNentry.grid(row=1,column=1)
+tolNentry.insert(0,'10')
