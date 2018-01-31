8e4e70b61093cbc56209195774c9a78d4d54d92a
huangyu2 <huangyu2@pset.suntec.net>
2018-01-31 19:23:12 +0800

    tensorflow

---
+++ tensorflow/test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include "TestMode.h"
+int main()
+{
+    TestMode* t=new TestMode();
+    for(int i=0;i<t->len;i++)
+    {
+        //std::cout<<t->data[i][COLUMN-1]<<std::endl;
+    }
+}
