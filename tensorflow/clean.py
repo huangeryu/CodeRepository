9f2032d1b61b6f7ba0c5bd5ae6e7c6393c632c3a
huangeryu <tao2010abc@163.com>
2018-02-09 17:16:20 +0800

    modified

--- tensorflow/clean.py
+++ tensorflow/clean.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #encoding:utf-8
-
+from __future__ import print_function
 with open('./test','rb') as fr:
-    print tuple(bytearray(fr.read(4)))==(0x7f,0x45,0x4c,0x46)
+    print(tuple(bytearray(fr.read(4)))==(0x7f,0x45,0x4c,0x46))
 
